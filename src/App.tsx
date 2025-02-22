39db9d29bf690ad0148bdca7e672893d7414a65c
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-23 01:26:43 +0900

    shadcn tailwindcss v4에 대응하는 canary버전 설치, 버그 해결(css파일에 세미콜론 두 개 들어가는 버그 있음.)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css"
+import { Button } from "./components/ui/button"
 export function App() {
   return (
     <div className="text-red-600">
-      텍스트
+      <Button>ddnpm</Button>
     </div>
   )
 }
