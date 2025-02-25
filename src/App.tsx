5f9ea160a2c7afe8398c2fa60326ecc3c941deae
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-26 02:54:43 +0900

    Room 컴포넌트 App으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { ReactNode } from "react";
 import "./App.css"
-import Room from "./pages/room/room"
+import Canvas from "./pages/room/canvas";
+import TextEditor from "./pages/room/editor";
+import { WebSocketProvider } from "./provider/websocket-provider";
+import { Provider } from 'react-redux'
+import { store } from "./store/store";
 export function App() {
   return (
-    <Room></Room>
+      <Provider store={store}>
+        <WebSocketProvider>
+          <AppContainer>
+            <Canvas></Canvas>
+            <EditorArea>
+              <TextEditor direction="top"></TextEditor>
+              <TextEditor direction="bottom"></TextEditor>
+            </EditorArea>
+          </AppContainer>
+        </WebSocketProvider>
+      </Provider>
+  );
+}
+  
+function AppContainer({children}: {children: ReactNode}) {
+  return (
+    <div className="w-full h-screen flex">
+      {children}
+    </div>
+
+  )
+}
+
+function EditorArea({children}: {children: ReactNode}) {
+  return (
+    <div className="w-full h-full flex flex-col">
+      {children}
+    </div>
   )
 }
