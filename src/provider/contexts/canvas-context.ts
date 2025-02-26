d97f6518200bf4082755d0dce0fc80c65224e1dc
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-27 02:41:04 +0900

    Redux 도입으로 인한 Context 삭제

--- src/provider/contexts/canvas-context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import { CanvasData } from "shared-coding-gather";
-
-type CanvasContextType = {
-  canvas: CanvasData
-}
-export const CanvasContext = createContext<CanvasContextType | null>(null);
--- src/provider/contexts/editors-context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { EditorDatas } from "shared-coding-gather";
-
-type EditorsContextType = {
-  editor: EditorDatas
-}
-
-export const EditorsContext = createContext<EditorsContextType | null>(null);
