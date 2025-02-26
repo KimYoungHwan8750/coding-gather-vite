d38238ddbfe47d1caae0d0be65e7ca4488e415a9
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-27 02:43:08 +0900

    Editor, Canvas를 Redux 구조상 데이터클래스로 변경, 메서드는 Behavior 클래스로 분리

--- src/store/canvas-slice.ts
+++ src/store/canvas-slice.ts
@@ -1,12 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CanvasData, ToolsType } from "shared-coding-gather";
 
-type StateType = {
-  canvas: CanvasData
-}
 
-const initialState: StateType = {
-  canvas: new CanvasData()
+
+const initialState: CanvasData = {
+  url: "",
+  tool: "cursor",
+  pending: false
 }
 
 const canvasSlice = createSlice({
@@ -14,25 +14,16 @@ const canvasSlice = createSlice({
   initialState,
   reducers: {
     setUrl(state, action: PayloadAction<string>) {
-      state.canvas = state.canvas.withUrl(action.payload);
-    },
-    setBinary(state, action: PayloadAction<Uint8Array>) {
-      state.canvas = state.canvas.withBinary(action.payload);
+      state.url = action.payload;
     },
     setTools(state, action: PayloadAction<ToolsType>) {
-      state.canvas = state.canvas.withTool(action.payload);
+      state.tool = action.payload;
     },
     setPending(state, action: PayloadAction<boolean>) {
-      state.canvas = state.canvas.withPending(action.payload);
+      state.pending = action.payload;
     },
-    setBase64(state, action: PayloadAction<string>) {
-      state.canvas = state.canvas.withBase64Data(action.payload);
-    },
-    setBase64FromBinary(state, action: PayloadAction<Uint8Array>) {
-      state.canvas = state.canvas.withBase64Data(state.canvas.encodeBinaryToBase64(action.payload));
-    }
   }
 })
 
-export const { setUrl, setBinary, setTools, setPending, setBase64, setBase64FromBinary } = canvasSlice.actions;
+export const { setUrl, setTools, setPending } = canvasSlice.actions;
 export default canvasSlice.reducer;
--- src/store/editors-slice.ts
+++ src/store/editors-slice.ts
@@ -1,14 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { AppConstant, ChangeLanguageResponse, EditorData, InputTextResponse } from "shared-coding-gather"
+import { AppConstant, ChangeLanguageResponse, DirectionType, EditorController, EditorControllers, EditorData, InputTextResponse } from "shared-coding-gather"
 
 type StateType = {
-  topEditor: EditorData
-  bottomEditor: EditorData
+  topEditorData: EditorData
+  bottomEditorData: EditorData
 }
-
 const initialState: StateType = {
-  topEditor: new EditorData(AppConstant.direction.TOP, AppConstant.language.PLAIN_TEXT, ""),
-  bottomEditor: new EditorData(AppConstant.direction.BOTTOM, AppConstant.language.PLAIN_TEXT, "")
+  topEditorData: {
+    direction: AppConstant.direction.TOP,
+    language: AppConstant.language.PLAIN_TEXT,
+    text: ""
+  },
+  bottomEditorData: {
+    direction: AppConstant.direction.BOTTOM,
+    language: AppConstant.language.PLAIN_TEXT,
+    text: ""
+  }
 }
 
 const editorsSlice = createSlice({
@@ -20,14 +27,14 @@ const editorsSlice = createSlice({
      * @description payload를 전달하면 해당 방향의 에디터에 텍스트를 설정
      */
     setText(state, action: PayloadAction<InputTextResponse>) {
-      action.payload.direction === AppConstant.direction.TOP ? state.topEditor = state.topEditor.withText(action.payload.text) : state.bottomEditor = state.bottomEditor.withText(action.payload.text);
+      action.payload.direction === AppConstant.direction.TOP ? state.topEditorData.text = action.payload.text : state.bottomEditorData.text = action.payload.text;
     },
 
     /**
      * @description payload를 전달하면 해당 방향의 에디터에 언어를 설정
      */
     setLanguage(state, action: PayloadAction<ChangeLanguageResponse>) {
-      action.payload.direction === AppConstant.direction.TOP ? state.topEditor = state.topEditor.withLanguage(action.payload.language) : state.bottomEditor = state.bottomEditor.withLanguage(action.payload.language);
+      action.payload.direction === AppConstant.direction.TOP ? state.topEditorData.language = action.payload.language : state.bottomEditorData.language = action.payload.language;
     }
   }
 })
