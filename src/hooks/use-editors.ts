1285765477b3f62a2683a55b4a36cf318d92546f
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-28 23:19:41 +0900

    selector 직접 export하게 변경, selector를 반환하던 custom 훅은 삭제

--- src/hooks/use-editors.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from "@/store/store";
-import { useSelector } from "react-redux";
-
-export function useEditorsSelector() {
-  const selector = useSelector((state: RootState) => state.editors);
-  return selector;
-}
