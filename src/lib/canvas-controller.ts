6e36d4e2600dd6ac8365874d3d955e921ff58614
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-03-03 23:12:04 +0900

    hand, pencil, eraser, zoomin, zoomout 기능 구현 중. zoomin, zoomout시 pencil 원래 위치 보존이 안 되고 있어서 작업해야함

--- src/lib/canvas-controller.ts
+++ src/lib/canvas-controller.ts
@@ -1,5 +1,5 @@
-import { ToolsType } from "node_modules/shared-coding-gather/dist/types/constant";
 import { CanvasLogic } from "./canvas-logic";
+import { ZoomLevelType, CanvasMouseState, AppConstant, ToolsType } from "shared-coding-gather";
 
 type FieldPositionType = {
   x1: number
@@ -15,15 +15,22 @@ type DestinationPositionType = {
   y2: number
 }
 
-export type CanvasMouseState = "idle" | "down" | "move" | "up";
 export class CanvasController {
   private readonly destination: DestinationPositionType;
   private readonly field: FieldPositionType;
-
+  cursorSVG = `
+<svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="5" cy="5" r="5" fill="white" stroke="black" stroke-width="1"/>
+</svg>
+`;
   canvasLogic: CanvasLogic = new CanvasLogic();
   ctx: CanvasRenderingContext2D | null = null;
+  drawingCtx: CanvasRenderingContext2D | null = null;
   mouseState: CanvasMouseState = "up";
   tools: ToolsType = "hand";
+  strokeWidth: number = 4;
+  strokeColor: string = "white";
+  imageData: ImageData | undefined = undefined;
 
   mousedownCallback: (evt: MouseEvent) => void = () => {};
   mousemoveCallback: (evt: MouseEvent) => void = () => {};
@@ -46,17 +53,41 @@ export class CanvasController {
    */
   init() {
     this.ctx = this.canvas.getContext("2d");
+    this.drawingCtx = this.drawingCanvas.getContext("2d");
+    const tempCanvas = document.createElement("canvas");
+    const tempCtx = tempCanvas.getContext("2d");
+    
     if (!this.ctx) {
       throw new Error("2d context가 지원되지 않습니다.");
     }
+    if (!this.drawingCtx) {
+      throw new Error("그리기 2d context가 지원되지 않습니다.");
+    }
 
-    this.container.addEventListener("mousedown", this.mousedownCallback);
-    this.container.addEventListener("mousemove", this.mousemoveCallback);
-    this.container.addEventListener("mouseup", this.mouseupCallback);
+    if(!tempCtx) return;
+    tempCanvas.width = this.canvas.width;
+    tempCanvas.height = this.canvas.height;
+    tempCtx.drawImage(this.drawingCanvas, 0, 0);
+    this.drawingCanvas.width = this.canvas.width
+    this.drawingCanvas.height = this.canvas.height
+    this.drawingCtx?.drawImage(tempCanvas, 0, 0, this.canvas.width, this.canvas.height);
+    
+    this.container.addEventListener("pointerdown", this.mousedownCallback);
+    this.container.addEventListener("pointermove", this.mousemoveCallback);
+    this.container.addEventListener("pointerup", this.mouseupCallback);
     // 드래그중 요소를 벗어났다가 들어왔을 때 드래그 이벤트 유지되는 현상 방지
-    this.container.addEventListener("mouseleave", this.mouseupCallback);
+    this.container.addEventListener("pointerleave", this.mouseupCallback);
     this.hand();
   }
+
+  cleanup() {
+    this.container.removeEventListener("pointerdown", this.mousedownCallback);
+    this.container.removeEventListener("pointermove", this.mousemoveCallback);
+    this.container.removeEventListener("pointerup", this.mouseupCallback);
+    this.container.removeEventListener("pointerleave", this.mouseupCallback);
+  }
+
+  
 
   setState(state: ToolsType) {
     switch (state) {
@@ -68,12 +99,6 @@ export class CanvasController {
         break;
       case "eraser":
         this.eraser();
-        break;
-      case "zoomIn":
-        this.zoomIn();
-        break;
-      case "zoomOut":
-        this.zoomOut();
         break;
       case "scissors":
         this.scissors();
@@ -91,42 +116,72 @@ export class CanvasController {
   }
 
   hand() {
+    this.setCursor("grab");
+
     this.reassignEventListener("down", this.mousedownCallback, () => {
       this.canvasLogic.handDown();
     });
 
     this.reassignEventListener("move", this.mousemoveCallback, (evt: MouseEvent) => {
       if(this.canvasLogic.getMouseInfo().isDragging) {
-      this.container.scrollBy({
-          top: -(evt.clientY - this.canvasLogic.getMouseInfo().currentY) / 1,
-          left: -(evt.clientX - this.canvasLogic.getMouseInfo().currentX) / 1,
-        });
-        this.container.style.cursor = "grabbing";
-      }
-      // 조건문 안에 들어가게 되면 최근 마우스 위치 동기화가 안 돼서 밖으로 뺌
-      this.canvasLogic.handMove(evt.clientX, evt.clientY);
+        const adjustMoveRatio = 1;
+        // 마우스 진행 방향과 반대로 이미지를 이동시켜서 당기는 효과 주기 위한 변수
+        const invertNumber = -1;
+        this.container.scrollBy({
+            top: (evt.clientY - this.canvasLogic.getMouseInfo().currentY) * adjustMoveRatio * invertNumber,
+            left: (evt.clientX - this.canvasLogic.getMouseInfo().currentX) * adjustMoveRatio * invertNumber,
+          });
+          this.setCursor("grabbing");
+        }
+        // 조건문 안에 들어가게 되면 최근 마우스 위치 동기화가 안 돼서 밖으로 빼야함함
+        this.canvasLogic.handMove(evt.clientX, evt.clientY);
   });
 
     this.reassignEventListener("up", this.mouseupCallback, () => {
       this.canvasLogic.handUp();
-      this.container.style.cursor = "grab";
+      this.setCursor("grab");
     });
 
   }
 
   pencil() {
+    this.setDrawingPointer({size: this.strokeWidth, color: "white"})
+
     this.reassignEventListener("down", this.mousedownCallback, (evt: MouseEvent) => {
       this.canvasLogic.pencilDown();
+      this.draw({offsetX: evt.offsetX, offsetY: evt.offsetY});
     });
 
     this.reassignEventListener("move", this.mousemoveCallback, (evt: MouseEvent) => {
       this.canvasLogic.pencilMove(evt.offsetX, evt.offsetY);
+      if(this.canvasLogic.getMouseInfo().isDragging) {
+        this.draw({offsetX: evt.offsetX, offsetY: evt.offsetY});
+      }
     });
 
     this.reassignEventListener("up", this.mouseupCallback, () => {
       this.canvasLogic.handUp();
+      this.setDrawingPointer({size: this.strokeWidth, color: "white"})
     });
 
+  }
+
+  draw({offsetX, offsetY}: {offsetX: number, offsetY: number}) {
+    this.setDrawingPointer({size: this.strokeWidth, color: this.strokeColor});
+    this.drawingCtx!.globalCompositeOperation = "source-over";
+    this.drawingCtx?.beginPath();
+    this.drawingCtx?.arc(offsetX, offsetY, this.strokeWidth, 0, 2 * Math.PI);
+    this.drawingCtx?.fill();
+  }
+
+  setStrokeWidth(size: number) {
+    this.strokeWidth = size;
+    this.setDrawingPointer({size: size});
+  }
+
+  setStrokeColor(color: string) {
+    this.strokeColor = color;
+    this.drawingCtx!.fillStyle = color;
   }
 
   eraser() {
@@ -136,6 +191,9 @@ export class CanvasController {
 
     this.reassignEventListener("move", this.mousemoveCallback, (evt: MouseEvent) => {
       this.canvasLogic.eraserMove(evt.offsetX, evt.offsetY);
+      if(this.canvasLogic.getMouseInfo().isDragging) {
+        this.erase({offsetX: evt.offsetX, offsetY: evt.offsetY});
+      }
     });
 
     this.reassignEventListener("up", this.mouseupCallback, () => {
@@ -143,12 +201,29 @@ export class CanvasController {
     });
   }
 
-  zoomIn() {
-    
+  erase({offsetX, offsetY}: {offsetX: number, offsetY: number}) {
+    this.setDrawingPointer({size: this.strokeWidth, color: "red"});
+    this.drawingCtx!.globalCompositeOperation = "destination-out";
+    this.drawingCtx?.beginPath();
+    this.drawingCtx?.arc(offsetX, offsetY, this.strokeWidth, 0, 2 * Math.PI);
+    this.drawingCtx?.fill();
   }
 
-  zoomOut() {
+  zoom(zoomLevel: ZoomLevelType) {
+    console.log(zoomLevel)
+    if(zoomLevel > AppConstant.maxZoomLevel) return;
+    if(zoomLevel < AppConstant.minZoomLevel) return;
+    // zoomLevel당 20%씩 확대
+    const zoomRatio = 0.2;
 
+    let scale = 1 + zoomLevel * zoomRatio;
+    this.ctx?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.canvas.width = this.imageBitmap.width * scale;
+    this.canvas.height = this.imageBitmap.height * scale;
+    // this.drawingCanvas.width = this.imageBitmap.width * scale;
+    // this.drawingCanvas.height = this.imageBitmap.height * scale;
+    this.ctx?.drawImage(this.imageBitmap, 0, 0, this.imageBitmap.width * scale, this.imageBitmap.height * scale);
+    
   }
 
   scissors() {
@@ -169,20 +244,46 @@ export class CanvasController {
 
   reassignEventListener(state: CanvasMouseState, oldEvent: any, newEvent: any) {
     if(state === "down") {
-      this.container.removeEventListener("mousedown", oldEvent);
-      this.container.addEventListener("mousedown", newEvent);
+      this.container.removeEventListener("pointerdown", oldEvent);
+      this.container.addEventListener("pointerdown", newEvent);
       this.mousedownCallback = newEvent;
     } else if(state === "move") {
-      this.container.removeEventListener("mousemove", oldEvent);
-      this.container.addEventListener("mousemove", newEvent);
+      this.container.removeEventListener("pointermove", oldEvent);
+      this.container.addEventListener("pointermove", newEvent);
       this.mousemoveCallback = newEvent;
     } else if(state === "up") {
-      this.container.removeEventListener("mouseup", oldEvent);
-      this.container.removeEventListener("mouseleave", oldEvent);
-      this.container.addEventListener("mouseup", newEvent);
-      this.container.addEventListener("mouseleave", newEvent);
+      this.container.removeEventListener("pointerup", oldEvent);
+      this.container.removeEventListener("pointerleave", oldEvent);
+      this.container.addEventListener("pointerup", newEvent);
+      this.container.addEventListener("pointerleave", newEvent);
       this.mouseupCallback = newEvent;
     }
+  }
+
+  /**
+   * @param cursor https://developer.mozilla.org/docs/Web/CSS/cursor 참조
+   */
+  setCursor(cursor: string) {
+    this.container.style.cursor = cursor;
+  }
+
+  private convertSvgToDataUrl(svg: string) {
+    const encodedSVG = window.encodeURIComponent(svg);
+    const dataURL = `data:image/svg+xml,${encodedSVG}`;
+    return dataURL;
+  }
+
+  setDrawingPointer({size, color}: {size: number, color?: string}) {
+    const containerSize = size * 2 + 2;
+    const strokeWidth = size > 3 ? 2 : 1;
+    const sizeWithStroke = size + (strokeWidth / 2);
+    const cursorSVG = `
+<svg width="${containerSize}" height="${containerSize}" viewBox="0 0 ${containerSize} ${containerSize}" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="${sizeWithStroke}" cy="${sizeWithStroke}" r="${size}" fill="${color || "white"}" stroke="black" stroke-width="${strokeWidth}"/>
+</svg>
+`;
+    this.cursorSVG = cursorSVG;
+    this.setCursor(`url(${this.convertSvgToDataUrl(cursorSVG)}) ${size} ${size}, auto`);
   }
 
 }
